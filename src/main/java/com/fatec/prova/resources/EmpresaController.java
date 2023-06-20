35c2ed95ce67a087462013a05a1ed652c6916815
felipecruz159 <felipe.cruz159@hotmail.com>
2023-06-20 09:31:40 -0300

    recuperar empresas

--- src/main/java/com/fatec/prova/resources/EmpresaController.java
+++ src/main/java/com/fatec/prova/resources/EmpresaController.java
@@ -1,10 +1,13 @@
 package com.fatec.prova.resources;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,4 +37,18 @@ public class EmpresaController {
 
             return ResponseEntity.created(location).body(newEmpresa);
     }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Empresa> getEmpresa(@PathVariable int id) {
+        Empresa empresa = empresaService.getEmpresaById(id);
+        return ResponseEntity.ok().body(empresa);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Empresa>> getEmpresas(){
+        List<Empresa> empresas = empresaService.getEmpresas();
+        return ResponseEntity.ok().body(empresas);
+    }
+
+    
 }
--- src/main/java/com/fatec/prova/services/EmpresaService.java
+++ src/main/java/com/fatec/prova/services/EmpresaService.java
@@ -1,10 +1,14 @@
 package com.fatec.prova.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fatec.prova.entities.Empresa;
 import com.fatec.prova.repositories.EmpresaRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 
 @Service
@@ -15,5 +19,15 @@ public class EmpresaService {
 
     public Empresa save(Empresa empresa){
         return empresaRepository.save(empresa);
+    }
+
+    public Empresa getEmpresaById(int id){
+        return empresaRepository.findById(id).orElseThrow(
+            () -> new EntityNotFoundException("Fornecedor não encontrado!")
+        );
+    }
+
+    public List<Empresa> getEmpresas(){
+        return empresaRepository.findAll();
     }
 }
